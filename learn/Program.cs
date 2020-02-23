c4e9e9f2b512f04295f9143bf3b4f3955374ce52
Алексей Воронков <alexey32101231@gmail.com>
2020-02-23 21:18:49 +0400

    Progress for 23.02

--- learn/Program.cs
+++ learn/Program.cs
@@ -10,7 +10,52 @@ namespace learn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Change by 23.02");
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Your name:{0} \nYour age:{1}",name,age);
+
+            Console.ReadKey();
+            // Попробуем создать массивы и поработать с ними
+            Console.Clear();
+            int [] marks = new int[] {1,2,3,4,5};
+            Console.Write("In school u can get marks: ");
+            foreach(int i in marks)
+                Console.Write($"{i}"+" ");
+
+            Console.ReadKey();
+            // А сейчас попробуем написать соритровку пузырьком для введенного с клавиатуры массива
+            Console.Clear();
+            int [] mas = new int[5];
+            Console.WriteLine("Enter 5 values in array: ");
+            for(int i = 0; i < mas.Length; i++){
+                Console.Write("{0}'s value: ", i+1);
+                mas[i] = Int32.Parse(Console.ReadLine());
+            }
+            //Console.ReadKey();
+            Console.WriteLine("Entered array: ");
+            for(int i = 0;i < mas.Length; i++)
+                Console.Write(mas[i]+ " ");
+            Console.ReadKey();
+            
+            BubbleSort(mas);
+            Console.WriteLine();
+            Console.WriteLine("Sorted array: ");
+            for(int i = 0;i < mas.Length; i++)
+                Console.Write(mas[i]+ " ");
+        }
+
+        static void BubbleSort(int[] array){
+            int temp;
+            for(int i=0; i < array.Length-1; i++)
+                for(int j=0; j < array.Length-1;j++)
+                    if(array[j]>array[j+1]){
+                        temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
         }
     }
 }
