20b278e4cd507ea450547f1c8048295000f46912
Алексей Воронков <alexey32101231@gmail.com>
2020-02-26 19:23:10 +0400

    Finally I have a good(how I think) structure for start 3rd chapter on metanit.com

---
+++ 3rd Chapter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3rd_Chapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
