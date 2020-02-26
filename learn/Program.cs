f2aeedf185b8b48360f7226512d41d09ce1c4621
Алексей Воронков <alexey32101231@gmail.com>
2020-02-26 19:44:26 +0400

    1 more try

--- learn/Program.cs
+++ learn/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace learn
 {
-    class Chapter2
+    class Program
     {
         static void Main(string[] args)
         {
